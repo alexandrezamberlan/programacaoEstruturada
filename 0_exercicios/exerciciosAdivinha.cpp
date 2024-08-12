2f8f0f128683047e5386fb124f4bae3c3166f41e
Alexandre de O. Zamberlan <alexandre.o.zamberlan@gmail.com>
2024-08-12 20:57:36 -0300

    Update exerciciosAdivinha.cpp

--- 0_exercicios/exerciciosAdivinha.cpp
+++ 0_exercicios/exerciciosAdivinha.cpp
@@ -1,47 +1,32 @@
-/*
-Fazer um programa em C ou C++ que popule e mostre um vetor com números inteiros e aleatórios. A quantidade de números deve ser igual ao tamanho do vetor inteiro definido. Em seguida, o programa deve mostrar todos os números que aparecem mais de 2 vezes no vetor.
-*/
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
+#include <string>
 #include <vector>
-
+#define TAMANHO 5
 using namespace std;
+
 int main() {
-    system("cls");
-    int tamanho;
-    cout << "Digite o tamanho do vetor: ";
-    cin >> tamanho;
+    vector<string> nomes(TAMANHO);
 
-    // Cria um vetor de inteiros com o tamanho definido
-    vector<int> vetor(tamanho);
-
-    srand(time(NULL));
-    for (int i = 0; i < tamanho; i++) {
-        vetor[i] = rand() % 10;
-        cout << vetor[i] << " ";
+    for (int i = 0; i < TAMANHO; i++) {
+        cout << "Digite o nome " << i+1 << ": ";
+        cin >> nomes[i];
     }
 
-    int contaOcorrencia;
-    int numeroAnalisado;
-    for (int i = 0; i < tamanho; i++) {
-        if (vetor[i] != -27) {
-            numeroAnalisado = vetor[i];
-            contaOcorrencia = 1;
-            for (int j = i+1; j < tamanho; j++) {
-                if (numeroAnalisado == vetor[j]) {
-                    contaOcorrencia++;
-                    if (contaOcorrencia > 1) {
-                        vetor[j] = -27;
-                    }
-                }
-            }
-            if (contaOcorrencia > 1){
-                cout << vetor[i] << " aparece " << contaOcorrencia << " vezes\n";
+    bool houveTroca = true;
+    while (houveTroca) {
+        houveTroca = false;
+        for (int i = 0; i < TAMANHO-1; i++) {
+            if (nomes[i] > nomes[i+1]) {
+                string aux = nomes[i];
+                nomes[i] = nomes[i+1];
+                nomes[i+1] = aux;
+                houveTroca = true;
             }
         }
     }
 
-    cout << endl;
+    cout << nomes[0] << endl;
+    cout << nomes[TAMANHO-1] << endl;
+
     return 1;
 }
