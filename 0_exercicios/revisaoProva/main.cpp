bd878bb14efb53524509f9b990246272e78c2b63
Alexandre de O. Zamberlan <alexandre.o.zamberlan@gmail.com>
2024-09-02 21:30:52 -0300

    Update main.cpp

--- 0_exercicios/revisaoProva/main.cpp
+++ 0_exercicios/revisaoProva/main.cpp
@@ -11,4 +11,22 @@ int main() {
 
     popularVetor(vetor1, TAMANHO);
     exibirVetor(vetor1, TAMANHO);
+
+    escreverMenor(vetor1, TAMANHO);
+    cout << "Funcao: o menor valor eh: " << pegarMenor(vetor1, TAMANHO) << endl;
+
+    escreverSoma(vetor1, TAMANHO);
+    cout << "Funcao: a soma dos elementos eh: " << somar(vetor1, TAMANHO) << endl;
+
+    popularVetor(vetor2, TAMANHO);
+    substituirPares(vetor2, TAMANHO);
+    exibirVetor(vetor2, TAMANHO);
+
+    //Copiando o conteudo do vetor1 para o vetor2
+    cout << "Copiando vetor....\n";
+    copiarVetor(vetor1,vetor2,TAMANHO);
+    cout << "Vetor1\n";
+    exibirVetor(vetor2, TAMANHO);
+    cout << "Vetor2\n";
+    exibirVetor(vetor2, TAMANHO);
 }
